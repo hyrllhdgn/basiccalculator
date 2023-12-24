20a9416b7c5848d5f34a747bde39c836784cb960
Hayrullah Dogan <hyrllhdgn@gmail.com>
2023-12-24 19:22:39 +0300

    BasicCalculator
    My first swift code.

--- basiccalculator/ViewController.swift
+++ basiccalculator/ViewController.swift
@@ -8,11 +8,89 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+    @IBOutlet weak var firstNumber: UITextField!
+    
+    @IBOutlet weak var secondNumber: UITextField!
+    
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
+    
+    
+    override func viewDidLoad()
+    
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func toplamaTiklandi(_ sender: Any) {
+        
+        if let ilkSayi = Int(firstNumber.text!){
+            if let ikinciSayi = Int(secondNumber.text!) {
+                
+                let sonuc = ilkSayi + ikinciSayi
+                resultLabel.text = String(sonuc)
+                
+            }
+        }
+        
+    
+        
+        
+        
+    }
+    
+    @IBAction func cikarmaTiklandi(_ sender: Any) {
+        
+        
+        if let ilkSayi = Int(firstNumber.text!){
+            if let ikinciSayi = Int(secondNumber.text!) {
+                
+                let sonuc = ilkSayi - ikinciSayi
+                resultLabel.text = String(sonuc)
+                
+            }
+        }
+        
+    }
+    
+    
+    @IBAction func carpmaTiklandi(_ sender: Any) {
+        
+        if let ilkSayi = Int(firstNumber.text!){
+            if let ikinciSayi = Int(secondNumber.text!) {
+                
+                let sonuc = ilkSayi * ikinciSayi
+                resultLabel.text = String(sonuc)
+                
+            }
+        }
+        
+        
+        
+    }
+    
+    
+    @IBAction func bolmeTiklandi(_ sender: Any) {
+        
+        
+        if let ilkSayi = Int(firstNumber.text!){
+            if let ikinciSayi = Int(secondNumber.text!) {
+                
+                let sonuc = ilkSayi / ikinciSayi
+                resultLabel.text = String(sonuc)
+                
+            }
+        }
+        
+        
+        
+        
+    }
+    
+    
 
 
 }
